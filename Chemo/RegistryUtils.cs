525df588090dd74ee71ebaee8aa19c7de84302f4
Tom Richards <tom@tomrichards.net>
2019-04-04 22:05:23 -0400

    Simplify RegistryUtils

--- Chemo/RegistryUtils.cs
+++ Chemo/RegistryUtils.cs
@@ -7,23 +7,13 @@ namespace Chemo
         public static bool IntEquals(string keyName, string valueName, int expectedValue)
         {
             var value = Registry.GetValue(keyName, valueName, null);
-            if (value == null || (int)value != expectedValue)
-            {
-                return false;
-            }
-
-            return true;
+            return (value != null && (int)value == expectedValue);
         }
 
         public static bool StringEquals(string keyName, string valueName, string expectedValue)
         {
             var value = Registry.GetValue(keyName, valueName, null);
-            if (value == null || (string)value != expectedValue)
-            {
-                return false;
-            }
-
-            return true;
+            return (value != null && (string)value == expectedValue);
         }
     }
 }
