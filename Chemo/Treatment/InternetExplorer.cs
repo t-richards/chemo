8e9e19565abb2f1810db01b610da61e0e42d5846
Tom Richards <tom@tomrichards.net>
2019-04-04 20:54:14 -0400

    Refactor InternetExplorer

--- Chemo/Treatment/InternetExplorer.cs
+++ Chemo/Treatment/InternetExplorer.cs
@@ -6,50 +6,57 @@ namespace Chemo.Treatment
     class InternetExplorer : ITreatment
     {
         private static readonly Logger logger = Logger.Instance;
-        public const string IE32BitName = "Internet-Explorer-Optional-x86";
-        public const string IE64BitName = "Internet-Explorer-Optional-amd64";
+        public static readonly string IE32BitName = "Internet-Explorer-Optional-x86";
+        public static readonly string IE64BitName = "Internet-Explorer-Optional-amd64";
+        protected string resolvedFeatureName = "";
+
+        public InternetExplorer()
+        {
+            if (Environment.Is64BitOperatingSystem)
+            {
+                resolvedFeatureName = IE64BitName;
+            }
+            else
+            {
+                resolvedFeatureName = IE32BitName;
+            }
+        }
 
         public bool ShouldPerformTreatment()
         {
+            try
+            {
+                using (DismSession session = DismApi.OpenOnlineSession())
+                {
+                    DismFeatureInfo info = DismApi.GetFeatureInfo(session, resolvedFeatureName);
+                    if (
+                        info.FeatureState == DismPackageFeatureState.NotPresent ||
+                        info.FeatureState == DismPackageFeatureState.Removed ||
+                        info.FeatureState == DismPackageFeatureState.Staged ||
+                        info.FeatureState == DismPackageFeatureState.UninstallPending)
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
             return true;
         }
 
         public bool PerformTreatment()
         {
-            string featureName;
-            if (Environment.Is64BitOperatingSystem)
-            {
-                featureName = IE64BitName;
-            }
-            else
-            {
-                featureName = IE32BitName;
-            }
-
             try
             {
                 using (DismSession session = DismApi.OpenOnlineSession())
                 {
-                    DismFeatureInfo info = DismApi.GetFeatureInfo(session, featureName);
-                    if (
-                        info.FeatureState == DismPackageFeatureState.NotPresent ||
-                        info.FeatureState == DismPackageFeatureState.Removed ||
-                        info.FeatureState == DismPackageFeatureState.Staged)
-                    {
-                        logger.Log("Internet Explorer 11 is not present on the system.");
-                        return true;
-                    }
-
-                    if (info.FeatureState == DismPackageFeatureState.UninstallPending)
-                    {
-                        logger.Log("Internet Explorer 11 is already pending uninstall.");
-                        return true;
-                    }
-
                     try
                     {
                         logger.Log("Disabling Internet Explorer 11...");
-                        DismApi.DisableFeature(session, featureName, null, true);
+                        DismApi.DisableFeature(session, resolvedFeatureName, null, true);
                     }
                     catch (DismRebootRequiredException ex)
                     {
