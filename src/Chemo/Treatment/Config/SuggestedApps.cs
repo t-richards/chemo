0f0dba305bf4663a76b5aa6f7c41c7dfcafdbf4d
Tom Richards <tom@tomrichards.net>
2020-08-02 18:56:08 -0400

    Turn off suggested apps harder

--- src/Chemo/Treatment/Config/SuggestedApps.cs
+++ src/Chemo/Treatment/Config/SuggestedApps.cs
@@ -5,8 +5,19 @@ namespace Chemo.Treatment.Config
 {
     class SuggestedApps : BaseTreatment
     {
+        // Tiles
         private const string CloudContent = @"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Cloud Content";
-        private const int DesiredValue = 1;
+        private const string ConsumerFeaturesKey = "DisableWindowsConsumerFeatures";
+        private const int ConsumerFeaturesValue = 1;
+
+        // Suggestions v0
+        private const string ContentDeliveryManager = @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager";
+        private const string SuggestionsKey = "SystemPaneSuggestionsEnabled";
+        private const int SuggestionsValue = 0;
+
+        // Suggestions v1
+        private const string SubscribedContent = "SubscribedContent-338388Enabled";
+        private const int SubscribedContentValue = 0;
 
         public override string Name()
         {
@@ -20,14 +31,20 @@ namespace Chemo.Treatment.Config
 
         public override bool ShouldPerformTreatment()
         {
-            return !RegistryUtils.IntEquals(CloudContent, "DisableWindowsConsumerFeatures", DesiredValue);
+            return !(
+                RegistryUtils.IntEquals(CloudContent, ConsumerFeaturesKey, ConsumerFeaturesValue) &&
+                RegistryUtils.IntEquals(ContentDeliveryManager, SuggestionsKey, SuggestionsValue) &&
+                RegistryUtils.IntEquals(ContentDeliveryManager, SubscribedContent, SubscribedContentValue)
+            );
         }
 
         public override bool PerformTreatment()
         {
             try
             {
-                Registry.SetValue(CloudContent, "DisableWindowsConsumerFeatures", DesiredValue, RegistryValueKind.DWord);
+                Registry.SetValue(CloudContent, ConsumerFeaturesKey, ConsumerFeaturesValue, RegistryValueKind.DWord);
+                Registry.SetValue(ContentDeliveryManager, SuggestionsKey, SuggestionsValue, RegistryValueKind.DWord);
+                Registry.SetValue(ContentDeliveryManager, SubscribedContent, SubscribedContentValue, RegistryValueKind.DWord);
                 Logger.Log("Successfully turned off suggested apps.");
                 return true;
             }
